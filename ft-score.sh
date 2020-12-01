bf3bf5eb178bcda56a975ed9c3678c32e77b5962
Phil de Joux <philderbeast@gmail.com>
2020-12-01 09:13:30 -0500

    Rescore with ft-far-out.

--- ft-score.sh
+++ ft-score.sh
@@ -17,8 +17,15 @@ ft-peg-frame "$COMP"
 ft-align-time "$COMP"
 ft-discard-further "$COMP"
 ft-area-step "$COMP"
-ft-mask-track "$COMP"
+
+ft-mask-arrival "$COMP"
+ft-mask-bonus "$COMP"
+ft-mask-effort "$COMP"
+ft-mask-lead "$COMP"
+ft-mask-reach "$COMP"
+
 ft-land-out "$COMP"
+ft-far-out "$COMP"
 ft-gap-point "$COMP"
 
 ft-comp-serve "$COMP"
